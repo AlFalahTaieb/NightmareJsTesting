72a4231599d477a3b37f1ec9d33bd28256c9933f
AlFalah <taiebalfalah@gmail.com>
2019-03-10 21:42:05 +0100

    Try to understand

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+const nightmare = Nightmare({  openDevTools: {
+    mode: 'detach'
+  },
+  show: true
+})
 
 nightmare
-  .goto('https://duckduckgo.com')
-  .type('#search_form_input_homepage', 'github nightmare')
-  .click('#search_button_homepage')
-  .wait('#r1-0 a.result__a')
-  .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
-  .end()
-  .then(console.log)
+  .goto('https://searx.me/')
+//   .type('#form-control input-lg autofocus', 'github nightmare')
+//   .click('#search_button_homepage')
+//   .wait('#r1-0 a.result__a')
+//   .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+//   .end()
+//   .then(console.log)
   .catch(error => {
     console.error('Search failed:', error)
   })
