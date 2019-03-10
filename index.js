af92281246b6c9a1e0ae53c750c58a0822cd91dd
AlFalah <taiebalfalah@gmail.com>
2019-03-10 22:13:59 +0100

    Type working on searx

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({  openDevTools: {
-    mode: 'detach'
-  },
+const nightmare = Nightmare({  
+    // openDevTools: {mode: 'detach'},
   show: true
 })
 
 nightmare
   .goto('https://searx.me/')
-//   .type('#form-control input-lg autofocus', 'github nightmare')
+  .type('#search_form', 'github nightmare')
 //   .click('#search_button_homepage')
 //   .wait('#r1-0 a.result__a')
 //   .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
