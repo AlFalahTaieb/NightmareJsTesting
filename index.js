675c44990561e190094f7ca2a414ad304d2f8e82
AlFalah <taiebalfalah@gmail.com>
2019-03-11 10:27:46 +0100

    add Cli interaction

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({  
-    // openDevTools: {mode: 'detach'},
+const sourceFile = require('./secrets')
+const readline = require('readline');
+const nightmare = Nightmare({
+  // openDevTools: {mode: 'detach'},
   show: true
 })
 
-nightmare
-  .goto('https://searx.me/')
-  .type('#search_form', 'github nightmare')
-//   .click('#search_button_homepage')
-//   .wait('#r1-0 a.result__a')
-//   .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
-//   .end()
-//   .then(console.log)
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Where do you want to log in ? ', (answer) => {
+
+  switch (answer) {
+    case ('facebook'):
+      nightmare
+        .goto(`https://${answer}.com`)
+        .type('#email', sourceFile.email)
+        .type('#pass', sourceFile.password)
+        .click('#loginbutton')
+        .catch(error => {
+          console.error('Search failed:', error)
+        })
+
+
+
+      break
+    default:
+      console.log('Sorry, we are out of ' + answer + '.')
+
+  }
+
+
+  rl.close();
+});
